eb4f78b83da7612bebae186a3f29e8437c78dbbe
czaloj <czaloj@gmail.com>
2017-08-02 23:41:41 -0700

    Adding a new project to the repo.

---
+++ GameBuild/src/main.cpp
@@ -0,0 +1,6 @@
+#include "stdafx.h"
+
+// The entry point to the game
+int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+    return 0;
+}
---
+++ GameBuild/src/stdafx.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <Vorb/stdafx.h>
+
+#include <stdlib.h>
+#include <wchar.h>
+#include <stdio.h>
+#include <string>
+#include <memory>
+
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
